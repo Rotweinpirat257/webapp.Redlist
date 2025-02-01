07f0fa2e3f14fa0d1b2f7d5f642e24939806c5c9
DoctorBrain55 <s_sevincs22@stud.hwr-berlin.de>
2025-02-01 18:44:41 +0100

    feat: Änderungen an app.py und models.py, neue Datenbankdateien hinzugefügt

---
+++ instance/-- SQLite.sql
@@ -0,0 +1,2 @@
+-- SQLite
+UPDATE `group` SET name = "stacy1_Tester_Chat" WHERE name = "stacy1_Chat";
--- models.py
+++ models.py
@@ -1,1 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
 
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    last_page = db.Column(db.Integer, default=1)
+    groups = db.relationship('Group', secondary='user_groups', back_populates='users')
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, unique=True, nullable=False)  # TMDb movie ID
+    title = db.Column(db.String(100), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+
+class Matches(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+    status = db.Column(db.String(10), nullable=False)  # like  dislike
